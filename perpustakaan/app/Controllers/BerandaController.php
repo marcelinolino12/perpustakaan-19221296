e3566ac942bfbda30f378f20ccba5cd868f95698
marcelinolino12 <jojo110318@gmail.com>
2023-09-25 18:51:56 +0700

    menambahkan fitur beranda

---
+++ perpustakaan/app/Controllers/BerandaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BerandaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+    public function home()
+    {
+        //
+    }
+    public function about()
+    {
+        //
+    }
+    public function profil()
+    {
+        //
+    }
+}
