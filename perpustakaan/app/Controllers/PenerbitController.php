272b02e10c9833cc0ace40e07406c974e472726f
marcelinolino12 <jojo110318@gmail.com>
2023-09-25 19:57:34 +0700

    menambahkan fitur penerbit

---
+++ perpustakaan/app/Controllers/PenerbitController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PenerbitController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+    public function tahun_terbit()
+    {
+        //
+    } public function nama_penerbit()
+    {
+        //
+    }
+}
